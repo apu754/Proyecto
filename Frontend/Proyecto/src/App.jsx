d3eb82f6c7421b31d33082fdae78c132232b0759
apu754 <juandilbp@gmail.com>
2024-05-16 11:51:28 -0500

    algunos cambios con el navbar

--- Frontend/Proyecto/src/App.jsx
+++ Frontend/Proyecto/src/App.jsx
@@ -1,7 +1,6 @@
 // import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar.jsx';
-import ActionPage from './Pages/ActionPage.jsx';
 import HomePage from './Pages/HomePage.jsx';
 
 import LoginPage from './Login/Login.jsx';
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
-          <Route path='/actions' element={<ActionPage />} />
         </Routes>
       </div>
     </Router>
--- Frontend/Proyecto/src/Pages/HomePage.jsx
+++ Frontend/Proyecto/src/Pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import CountryList from '../Components/CountryList.jsx';
-import UserList from '../Components/UserList.jsx';
 import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div className='home-page container'>
       <h1>Acciones & Gestión S.A.S</h1>
-      <UserList />
+
       <CountryList />
     </div>
   );
