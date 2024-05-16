ef45509086361485f4020875ae880716e2e490a8
apu754 <juandilbp@gmail.com>
2024-05-16 16:18:37 -0500

    Quite el login

--- Frontend/Proyecto/src/App.jsx
+++ Frontend/Proyecto/src/App.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
+// import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar.jsx';
-import Login from './Login/Login.jsx';
 import ActionPage from './Pages/ActionPage.jsx';
 import HomePage from './Pages/HomePage.jsx';
 import UserPage from './Pages/UserPage.jsx';
 import './index.css';
 
 function App() {
-  const [isLoggedIn, setLoggedIn] = React.useState(false);
-
   return (
     <Router>
-      <NavBar isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn} />
+      <NavBar />
       <div className='container'>
         <Routes>
-          <Route path='/' element={isLoggedIn ? <HomePage /> : <Login setLoggedIn={setLoggedIn} />} />
-          <Route path='/users' element={isLoggedIn ? <UserPage /> : <Login setLoggedIn={setLoggedIn} />} />
-          <Route path='/actions' element={isLoggedIn ? <ActionPage /> : <Login setLoggedIn={setLoggedIn} />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/users' element={<UserPage />} />
+          <Route path='/actions' element={<ActionPage />} />
         </Routes>
       </div>
     </Router>
