5411b1348ce1cc9bee455bd2f479250e401e45f5
apu754 <juandilbp@gmail.com>
2024-05-28 13:30:32 -0500

    Backend

---
+++ Proyecto/BackEnd/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello word');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port:  ${port}`);
+});
