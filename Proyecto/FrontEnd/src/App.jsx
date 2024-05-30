f918f7ad2aa260958a153cd065d9f63ef04fa947
apu754 <juandilbp@gmail.com>
2024-05-30 16:12:17 -0500

    Actualizacion de error en el useState isRegistered

--- Proyecto/FrontEnd/src/App.jsx
+++ Proyecto/FrontEnd/src/App.jsx
@@ -12,28 +12,23 @@ import './index.css';
 function App() {
   // Recuperar el estado de inicio de sesión del usuario de localStorage
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
-  const [isRegistered, setIsRegistered] = useState(false); // Agrega estado para el registro
 
   // Función para actualizar el estado de inicio de sesión del usuario y guardar en localStorage
   const handleLogin = (value) => {
-    // Cambia el nombre de la función a handleLogin
     setIsLoggedIn(value);
     localStorage.setItem('isLoggedIn', value ? 'true' : 'false');
   };
 
   return (
     <Router>
-      <NavBar isLoggedIn={isLoggedIn} setLoggedIn={handleLogin} /> {/* Utiliza el nuevo nombre de la función */}
+      <NavBar isLoggedIn={isLoggedIn} setLoggedIn={handleLogin} />
       <div className='container'>
         <Routes>
-          <Route path='/' element={<HomePage isLoggedIn={isLoggedIn} setLoggedIn={handleLogin} />} />{' '}
-          {/* Utiliza el nuevo nombre de la función */}
-          <Route path='/login' element={<LoginPage setLoggedIn={handleLogin} />} />{' '}
-          {/* Utiliza el nuevo nombre de la función */}
-          <Route path='/register' element={<RegisterPage setRegistered={setIsRegistered} />} />{' '}
-          {/* Pasa setRegistered como prop */}
+          <Route path='/' element={<HomePage isLoggedIn={isLoggedIn} setLoggedIn={handleLogin} />} />
+          <Route path='/login' element={<LoginPage setLoggedIn={handleLogin} />} />
+          <Route path='/register' element={<RegisterPage />} />
           <Route element={<ProteccionRouter isLoggedIn={isLoggedIn} />}>
-            <Route path='/acciones' element={<Actions />} /> {/* Utiliza '/acciones' en lugar de '/Acciones' */}
+            <Route path='/acciones' element={<Actions />} />
             <Route path='/perfil' element={<Perfil />} />
           </Route>
           <Route path='*' element={<Navigate to={isLoggedIn ? '/acciones' : '/login'} />} />
